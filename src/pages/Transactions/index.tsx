ea283cc8d66cbf0bc8b4e3e7bebfb35fd95e12bf
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-15 20:01:01 -0300

    Create Transactions page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,14 @@
+import { TransactionsContainer } from './styles'
+
+export function Transactions() {
+  return (
+    <div>
+
+      <TransactionsContainer>
+        <h1>Transactions</h1>
+      </TransactionsContainer>
+    </div>
+  )
+}
+
+
