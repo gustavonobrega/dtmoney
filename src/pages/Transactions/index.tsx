7c8dec0d11b94952f18a6dd461a9eef5dde86d8f
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-15 20:13:48 -0300

    Implement Header component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from '../../components/Header'
 import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <div>
+      <Header />
 
       <TransactionsContainer>
         <h1>Transactions</h1>
