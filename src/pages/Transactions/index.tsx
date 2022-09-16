d80a213850981a7cc1e42cf9247ccbc86028f187
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-16 18:58:00 -0300

    Implement SearchForm component at Transaction page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { TransactionsContainer } from './styles'
+import { SearchForm } from './components/SearchForm'
+import { PriceHighligh, TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
   return (
@@ -9,7 +10,32 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
-        <h1>Transactions</h1>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighligh variant='income'>
+                  R$ 12.000,00
+                </PriceHighligh>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighligh variant='outcome'>
+                  - R$ 59,00 
+                </PriceHighligh>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   )
