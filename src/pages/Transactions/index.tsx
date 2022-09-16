c4d230fd941489f8cd04d5a7be3ad7271a099110
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-16 17:06:19 -0300

    Implement Summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
 
       <TransactionsContainer>
         <h1>Transactions</h1>
