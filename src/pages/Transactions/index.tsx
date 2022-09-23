8eacdacd5a206641743a8a12b94b4844310f2713
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-23 14:46:30 -0300

    Fetch json-server transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
+
 import { PriceHighligh, TransactionsContainer, TransactionsTable } from './styles'
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    async function  loadTransactions() {
+      const response = await fetch('http://localhost:3333/transactions');
+      const data = await response.json();
+
+      setTransactions(data);
+    }
+
+    loadTransactions();
+  }, [])
+  
   return (
     <div>
       <Header />
@@ -14,26 +38,18 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighligh variant='income'>
-                  R$ 12.000,00
-                </PriceHighligh>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighligh variant='outcome'>
-                  - R$ 59,00 
-                </PriceHighligh>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map(transaction => (
+              <tr key={transaction.id} >
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighligh variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighligh>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
